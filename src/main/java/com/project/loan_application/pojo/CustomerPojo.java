0ca63c9fc98deb0d14a86d700a6f1028db900bb7
TarunPoloju <tarunpoloju@gmail.com>
2025-01-23 14:32:04 +0530

    Changes to Pojo

--- src/main/java/com/project/loan_application/pojo/CustomerPojo.java
+++ src/main/java/com/project/loan_application/pojo/CustomerPojo.java
@@ -1,5 +1,17 @@
 package com.project.loan_application.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CustomerPojo{
-	
+	private int userId;        
+    private String username;   
+    private String email;      
+    private String password;  
+    private String fullName;   
+    private String phoneNumber;
 }
